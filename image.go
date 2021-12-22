77b687c80d4389a3b36b9203c94085fcb772d197
wanghaha-dev <52775415+develop1024@users.noreply.github.com>
2021-12-22 15:56:46 +0800

    update

--- image.go
+++ image.go
@@ -2,16 +2,45 @@ package u
 
 import (
 	"github.com/disintegration/imaging"
+	"sync"
 )
 
+type img struct {}
+
+var _img *img
+var _imgOnce sync.Once
+
+// Image 获取img对象
+func Image() *img {
+	_imgOnce.Do(func() {
+		_img = &img{}
+	})
+	return _img
+}
+
 // ResizePreserveAspectRatio 保持纵横比调整图片
-func ResizePreserveAspectRatio(width int, sourceImagePath string, savePath string) error {
+func (receiver *img)ResizePreserveAspectRatio(width int, sourceImagePath string, savePath string) error {
 	src, err := imaging.Open(sourceImagePath)
 	if err != nil {
 		return err
 	}
 
 	dstImage := imaging.Resize(src, width, 0, imaging.Lanczos)
+	err = imaging.Save(dstImage, savePath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Resize 调整图片
+func (receiver *img) Resize(sourceImagePath string, width, height int, savePath string) error {
+	src, err := imaging.Open(sourceImagePath)
+	if err != nil {
+		return err
+	}
+
+	dstImage := imaging.Resize(src, width, height, imaging.Lanczos)
 	err = imaging.Save(dstImage, savePath)
 	if err != nil {
 		return err
