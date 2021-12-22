617a701c456b49502344b06490781a5633ea6f61
wanghaha-dev <52775415+develop1024@users.noreply.github.com>
2021-12-22 15:16:16 +0800

    update

--- u.go
+++ u.go
@@ -204,6 +204,6 @@ func Delay(callback func()) {
 
 // Version 输出版本
 func Version() {
-	fmt.Println("v0.0.37")
-	fmt.Println("Last update time: 2021-12-22 15:11:00")
+	fmt.Println("v0.0.38")
+	fmt.Println("Last update time: 2021-12-22 15:16:00")
 }
