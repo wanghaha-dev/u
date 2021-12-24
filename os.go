64422016ba7e77004a6edcb887ef3085e906427a
wanghaha-dev <52775415+develop1024@users.noreply.github.com>
2021-12-24 17:38:32 +0800

    update

--- os.go
+++ os.go
@@ -1,6 +1,8 @@
 package u
 
 import (
+	"io"
+	"io/fs"
 	"os"
 	"path"
 	"path/filepath"
@@ -43,6 +45,78 @@ func (receiver *osObj)CheckCreateDirs(fPath string) error {
 		if err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+// CopyFile 复制文件或文件夹
+func (receiver *osObj)CopyFile(src, dst string) error {
+	srcData, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if srcData.IsDir() {
+		_, err := os.Stat(dst)
+		if os.IsNotExist(err) {
+			err := os.MkdirAll(dst, 0777)
+			if err != nil {
+				return err
+			}
+		}
+
+		err = filepath.Walk(src, func(srcFilePath string, info fs.FileInfo, err error) error {
+			if info.IsDir() {
+				return nil
+			}
+
+			dstFilename := path.Join(dst,
+				path.Join(
+					strings.TrimLeft(srcFilePath, src),
+					filepath.Base(srcFilePath),
+				),
+			)
+			err = receiver.copyFile(srcFilePath, dstFilename)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+	} else {
+		err := receiver.copyFile(src, dst)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// copyFile 复制文件
+func (receiver *osObj) copyFile(src, dst string) error {
+	err := OS().CheckCreateDirs(dst)
+	if err != nil {
+		return err
+	}
+
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+
+	defer dstFile.Close()
+	defer srcFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
 	}
 	return nil
 }
